a27ade24536466a2a04973efb5605e48731bd90b
Colin <harebovmakar@yandex.ru>
2023-03-17 20:08:33 +0500

    Добавьте файлы проекта.

---
+++ CristalOS/Kernel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace CristalOS
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine("Cristal booted successfully.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("CristalOS >> ");
+            string option = Console.ReadLine();
+        }
+    }
+}
